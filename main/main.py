99f45eefe6b865b1130f51a5a178e6ae28eb0ac6
Adamkohol <adam.vhenan@bk.ru>
2021-12-14 00:28:17 +0300

    Initial Commit

--- main/main.py
+++ main/main.py
@@ -125,6 +125,7 @@ async def cmd_block(message: types.Message):
     await message.answer("Вы заблокированы")
 
 
+
 @dp.errors_handler(exception=BotBlocked)
 async def error_bot_blocked(update: types.Update, exception: BotBlocked):
     # Update: объект события от Telegram. Exception: объект исключения
@@ -165,5 +166,5 @@ async def cmd_test1(message: types.Message):
 
 
 if __name__ == "__main__":
-    # Запуск бота
+
     executor.start_polling(dp, skip_updates=True)
