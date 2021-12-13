4c948a991988713a0e78e1880d756438b73b94ed
Adamkohol <adam.vhenan@bk.ru>
2021-12-14 00:30:13 +0300

    Initial Commit

--- main/main.py
+++ main/main.py
@@ -165,6 +165,7 @@ async def cmd_test1(message: types.Message):
         await message.answer("Я не понял вас. Попробуйте снова")
 
 
+
 if __name__ == "__main__":
 
     executor.start_polling(dp, skip_updates=True)
