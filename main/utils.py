2ba1b3aa6b4af930dd513d3e2d14cd24f6d0574e
Adamkohol <adam.vhenan@bk.ru>
2021-12-18 20:03:05 +0300

    Initial Commit

--- main/utils.py
+++ main/utils.py
@@ -1,7 +1,8 @@
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 class BotStates(StatesGroup):
-    Menu = State()
-    Dialog = State()
+    waiting_for_password = State()
