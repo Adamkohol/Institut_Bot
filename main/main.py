4b3c8b8e266802db564bc10212861f8941ff6ca5
Adamkohol <adam.vhenan@bk.ru>
2021-12-23 17:58:34 +0300

    POMOGASHA ZARABOTAL

--- main/main.py
+++ main/main.py
@@ -22,6 +22,8 @@ import pickle
 import tg_analytic
 from nltk.stem.lancaster import LancasterStemmer
 from Responses import chat
+import redis
+
 password = getenv("PASS_TOKEN_KEY")
 bot_token = getenv("BOT_TOKEN")
 if not bot_token:
@@ -35,13 +37,6 @@ logging.basicConfig(level=logging.INFO)
 AUTHORIZATION_STATE = False
 print("PROGRAM START")
 
-
-# пример обработчика ошибки
-# ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК
-
-
-
-# ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК
 
 # EXCEPTION FOR BLOCK---------------------------------------------------------------------------------------------------
 @dp.message_handler(commands="block")
@@ -59,8 +54,6 @@ async def error_bot_blocked(update: types.Update, exception: BotBlocked):
     # Такой хэндлер должен всегда возвращать True,
     # если дальнейшая обработка не требуется.
     return True
-
-
 # EXCEPTION FOR BLOCK---------------------------------------------------------------------------------------------------
 
 '''def is_emoji(s):
@@ -80,220 +73,159 @@ def text_has_emoji(text):
     return False
 
 
-# ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕР
-@dp.message_handler(commands="pcfat")  # проверка
-async def CHECKPASS(message: types.Message):  # await теперь обязателен
-    await message.answer("Введите пароль")
-    if message.text != "Отмена":
-        await check_password
-    else:
-        pass
 
-
-async def check_password(message: types.Message):
-    password_to_check = message.text
-
-    if password_to_check == password:
-        await message.answer("right")
-        AUTHORIZATION_STATE = True
-
-    elif message.text == "Отмена":
-        pass
-    else:
-        await message.answer("wrong")
-        AUTHORIZATION_STATE = False
-
-
-@dp.message_handler(commands="logout")
-async def OUT(message: types.Message):  # await теперь обязателен
-    await message.answer("Вы хотите выйти?")
-    if message.text != "Нет":
-        pass
-    elif message.text == "Да":
-        AUTHORIZATION_STATE = False
-
-
-# ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕР
-# СТАТУС СТУДЕНТ СТАТУС СТУДЕНТ СТАТУС СТУДЕНТ СТАТУС СТУДЕНТ СТАТУС СТУДЕНТ СТАТУС СТУДЕНТ СТАТУС СТУДЕНТ СТАТУС СТУДЕН
-if AUTHORIZATION_STATE == False:
-    @dp.message_handler(commands="start")  # новый обработчик
-    async def MENU(message: types.Message):  # await теперь обязателен
-        if AUTHORIZATION_STATE is False:
-            await message.answer("\t Здравствуй!🤖 Бот для учебы, вот список команд:"
-                                 " \n /start - начало работы \n /help - 🛠 помощь \n /faq - ⁉ часто задаваемые вопросы \n"
-                                 " /educationmaterial - 📚️ учебный материал \n"
-                                 " /testcheck - 🧩 тесты для самоподготовки\n", reply_markup=nav.mainMenu)
-        else:
-            await message.answer("\t Здравствуй!🤖 Бот для учебы, вот список команд:"
-                                 " \n /start - начало работы \n /help - 🛠 помощь \n /faq - ⁉ часто задаваемые вопросы \n"
-                                 " /remindlesson - 🔔 напомнить о занятии \n /reminddeadline - 🔔 напомнить о дедлайне \n "
-                                 "/educationresult - 🎓 успеваемость\n"
-                                 " /educationmaterial - 📚️ учебный материал \n"
-                                 " /testcheck - 🧩 тесты для самоподготовки\n"
-                                 " /congratsstudent -  🏆 топ учащихся\n", reply_markup=nav.mainMenu)
-
-
-    # Команды -------------------------------------------------------------------------------------------------------------
-
-    # Команды -------------------------------------------------------------------------------------------------------------
-    # Output функции ------------------------------------------------------------------------------------------------------
-    # чтобы работало и меню и команды
-    async def helpOut(message):
+async def BUTTON(message: types.Message):
+    if message.text == '🛠 Помощь':  # кнопочки, текст внутри message  не трогать
         await message.reply("help  is working")
+    elif message.text == '⁉ Частые вопросы':
+        await faqOut(message)
+    elif message.text == '🧩 Тесты':
+        await testcheckOut(message)
+    elif message.text == '🔔 Напомнить о занятии':
+        await remlesOut(message)
+    elif message.text == '🔔 Напомнить о дедлайне':
+        await remdeadOut(message)
+    elif message.text == '🎓 Успеваемость':
+        await eduresOut(message)
+    elif message.text == '📚️ Учебный материал':
+        await edumatOut(message)
+    elif message.text == '🏆 Топ':
+        await congstuOut(message)
+    elif message.text == 'Другое ➡':
+        await message.reply("Другое ➡", reply_markup=nav.otherMenu)
+    elif message.text == '⬅ Главное меню':
+        await message.reply("⬅ Главное меню", reply_markup=nav.mainMenu)
+
+# ВОЗМОЖНО ЛУЧШЕ ВЫНЕСТИ КНОПКИ В ФАЙЛ И ОТТУДА ИМПОРТИТЬ ЕСЛИ ЭТО ВОЗМОЖНО
+async def StudBUTTON(message: types.Message):
+    if message.text == '🛠 Помощь':  # кнопочки, текст внутри message  не трогать
+        await message.reply("help  is working")
+    elif message.text == '⁉ Частые вопросы':
+        await faqOut(message)
+    elif message.text == '🧩 Тесты':
+        await testcheckOut(message)
+    elif message.text == '📚️ Учебный материал':
+        await edumatOut(message)
+    elif message.text == 'Другое ➡':
+        await message.reply("Другое ➡", reply_markup=nav.otherMenu)
+    elif message.text == '⬅ Главное меню':
+        await message.reply("⬅ Главное меню", reply_markup=nav.mainMenu)
+
+     # async def helpOut(message):
+     #     await message.reply("help  is working")
+     #
+     #
+     # async def faqOut(message):
+     #     await message.reply("faq is working")
+     #
+     #
+     # async def remlesOut(message):
+     #     await message.reply("remind lesson is working")
+     #
+     #
+     # async def remdeadOut(message):
+     #     await message.reply("remind deadline is working")
+     #
+     #
+     # async def testcheckOut(message):
+     #     await message.reply("test is working:")
+     #
+     #
+     # async def eduresOut(message):
+     #     await message.reply("education result is working")
+     #
+     #
+     # async def edumatOut(message):
+     #     await message.reply("education material is working")
+     #
+     #
+     # async def congstuOut(message):
+     #     await message.reply("congrats is working")
+     #
 
 
-    async def faqOut(message):
-        await message.reply("faq is working")
+
+# ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕР
+
+@dp.message_handler(commands= 'go')
+async def start_handler(message: types.Message):
+    ids = [964783056, 720761725, 875674769, 608973271]
+    global AUTHORIZATION_STATE
+    if message.from_user.id not in ids:
+        AUTHORIZATION_STATE = False
+        await message.answer('Ошибся адресом, дружок')
+    else:
+        AUTHORIZATION_STATE = True
+        await message.answer("Привет, чем займёмся? :)")
 
 
-    async def remlesOut(message):
-        await message.reply("remind lesson is working")
+# ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕРКА ПАРОЛЯ ПРОВЕР
+@dp.message_handler(commands="start")  # новый обработчик
+async def MENU(message: types.Message):  # await теперь обязателен
+    if AUTHORIZATION_STATE == False:
+        await message.answer("\t Здравствуй!🤖 Бот для учебы, вот список команд:"
+                             " \n /start - начало работы \n /help - 🛠 помощь \n /faq - ⁉ часто задаваемые вопросы \n"
+                             " /educationmaterial - 📚️ учебный материал \n"
+                             " /testcheck - 🧩 тесты для самоподготовки\n", reply_markup=nav.mainMenu)
+    elif AUTHORIZATION_STATE == True:
+        await message.answer("\t Здравствуй!🤖 Бот для учебы, вот список команд:"
+                             " \n /start - начало работы \n /help - 🛠 помощь \n /faq - ⁉ часто задаваемые вопросы \n"
+                             " /remindlesson - 🔔 напомнить о занятии \n /reminddeadline - 🔔 напомнить о дедлайне \n "
+                             "/educationresult - 🎓 успеваемость\n"
+                             " /educationmaterial - 📚️ учебный материал \n"
+                             " /testcheck - 🧩 тесты для самоподготовки\n"
+                             " /congratsstudent -  🏆 топ учащихся\n", reply_markup=nav.mainMenu)
 
-
-    async def remdeadOut(message):
-        await message.reply("remind deadline is working")
-
-
-    async def testcheckOut(message):
-        await message.reply("test is working:")
-
-
-    async def eduresOut(message):
-        await message.reply("education result is working")
-
-
-    async def edumatOut(message):
-        await message.reply("education material is working")
-
-
-    async def congstuOut(message):
-        await message.reply("congrats is working")
-
-
-    # Output функции ---------------------------------------------------------------------------------------------------
-    # Кнопки -----------------------------------------------------------------------------------------------------------
-    async def StudBUTTON(message: types.Message):
-        if AUTHORIZATION_STATE is False:
-            if message.text == '🛠 Помощь':  # кнопочки, текст внутри message  не трогать
-                await message.reply("help  is working")
-            elif message.text == '⁉ Частые вопросы':
-                await faqOut(message)
-            elif message.text == '🧩 Тесты':
-                await testcheckOut(message)
-            elif message.text == '📚️ Учебный материал':
-                await edumatOut(message)
-            elif message.text == 'Другое ➡':
-                await message.reply("Другое ➡", reply_markup=nav.otherMenu)
-            elif message.text == '⬅ Главное меню':
-                await message.reply("⬅ Главное меню", reply_markup=nav.mainMenu)
-        else:
-            if message.text == '🛠 Помощь':  # кнопочки, текст внутри message  не трогать
-                await message.reply("help  is working")
-            elif message.text == '⁉ Частые вопросы':
-                await faqOut(message)
-            elif message.text == '🧩 Тесты':
-                await testcheckOut(message)
-            elif message.text == '🔔 Напомнить о занятии':
-                await remlesOut(message)
-            elif message.text == '🔔 Напомнить о дедлайне':
-                await remdeadOut(message)
-            elif message.text == '🎓 Успеваемость':
-                await eduresOut(message)
-            elif message.text == '📚️ Учебный материал':
-                await edumatOut(message)
-            elif message.text == '🏆 Топ':
-                await congstuOut(message)
-            elif message.text == 'Другое ➡':
-                await message.reply("Другое ➡", reply_markup=nav.otherMenu)
-            elif message.text == '⬅ Главное меню':
-                await message.reply("⬅ Главное меню", reply_markup=nav.mainMenu)
-
-
-    # СТАТУС СТУДЕНТ СТАТУС СТУДЕНТ СТАТУС СТУДЕНТ СТАТУС СТУДЕНТ СТАТУС СТУДЕНТ СТАТУС СТУДЕНТ СТАТУС СТУДЕНТ СТАТУС СТУДЕН
-
-    # СТАТУС ПРЕПОДАВАТЕЛЬ СТАТУС ПРЕПОДАВАТЕЛЬ СТАТУС ПРЕПОДАВАТЕЛЬ СТАТУС ПРЕПОДАВАТЕЛЬ СТАТУС ПРЕПОДАВАТЕЛЬ СТАТУС ПРЕПОД
-    # if AUTHORIZATION_STATE == True:
-    # @dp.message_handler(commands="start")  # новый обработчик
-    # async def MENU(message: types.Message):  # await теперь обязателен
-
-    # await message.answer("\t Здравствуй!🤖 Бот для учебы, вот список команд:"
-    #                    " \n /start - начало работы \n /help - 🛠 помощь \n /faq - ⁉ часто задаваемые вопросы \n"
-    #                   " /remindlesson - 🔔 напомнить о занятии \n /reminddeadline - 🔔 напомнить о дедлайне \n "
-    #                  "/educationresult - 🎓 успеваемость\n"
-    #                 " /educationmaterial - 📚️ учебный материал \n"
-    #                " /testcheck - 🧩 тесты для самоподготовки\n"
-    #               " /congratsstudent -  🏆 топ учащихся\n", reply_markup=nav.mainMenu)
-
-    # Команды -------------------------------------------------------------------------------------------------------------
-    @dp.message_handler(commands="help")  # напомнить о занятии
-    async def HELP(message: types.Message):  # await теперь обязателен
+@dp.message_handler(commands="help")  # напомнить о занятии
+async def HELP(message: types.Message):
+    if AUTHORIZATION_STATE == False: #студент
+        await shelpOut(message)
+    elif AUTHORIZATION_STATE == True: #препод
         await helpOut(message)
 
+@dp.message_handler(commands="remindlesson")  # напомнить о занятии
+async def REMINDLES(message: types.Message):
+    if AUTHORIZATION_STATE == False:  # студент
+        await message.answer('Ошибся адресом, дружок')
+    elif AUTHORIZATION_STATE == True:  # препод
+        await message.reply("remind lesson is working")
 
-    @dp.message_handler(commands="remindlesson")  # напомнить о занятии
-    async def FAQ(message: types.Message):  # await теперь обязателен
-        await faqOut(message)
-
-
-    @dp.message_handler(commands="remindlesson")  # напомнить о занятии
-    async def REMINDLES(message: types.Message):  # await теперь обязателен
-        await remlesOut(message)
-
-
-    @dp.message_handler(commands="reminddeadline")  # напомнить о дедлайне
-    async def REMINDED(message: types.Message):  # await теперь обязателен
+@dp.message_handler(commands="reminddeadline")  # напомнить о дедлайне
+async def REMINDED(message: types.Message):
+    if AUTHORIZATION_STATE == False:  # студент
+        await message.answer('Ошибся адресом, дружок')
+    elif AUTHORIZATION_STATE == True: # препод
         await remdeadOut(message)
 
-
-    @dp.message_handler(commands="testcheck")  # тесты для самопроверки
-    async def TESTCHECK(message: types.Message):  # await тeперь обязателен
+@dp.message_handler(commands="testcheck")  # тесты для самопроверки
+async def TESTCHECK(message: types.Message):
+    if AUTHORIZATION_STATE == False:  # студент
+        await stestcheckOut(message)
+    elif AUTHORIZATION_STATE == True:  # препод
         await testcheckOut(message)
 
-
-    @dp.message_handler(commands="educationresult")  # об успеваемости
-    async def EDRESUL(message: types.Message):  # await тeперь обязателен
+@dp.message_handler(commands="educationresult")  # об успеваемости
+async def EDRESUL(message: types.Message):
+    if AUTHORIZATION_STATE == False:  # студент
+        await message.answer('Ошибся адресом, дружок')
+    elif AUTHORIZATION_STATE == True:  # препод
         await eduresOut(message)
 
-
-    @dp.message_handler(commands="educationmaterial")  # учебный материал
-    async def EDMAT(message: types.Message):  # await тeперь обязателен
+@dp.message_handler(commands="educationmaterial")  # учебный материал
+async def EDMAT(message: types.Message):
+    if AUTHORIZATION_STATE == False:  # студент
+        await sedumatOut(message)
+    elif AUTHORIZATION_STATE == True:  # препод
         await edumatOut(message)
 
-
-    @dp.message_handler(commands="congratsstudent")  # поздравление если ты харош
-    async def CONGRATS(message: types.Message):  # await тeперь обязателен
+@dp.message_handler(commands="congratsstudent")  # поздравление если ты харош
+async def CONGRATS(message: types.Message):
+    if AUTHORIZATION_STATE == False:  # студент
+        await message.answer('Ошибся адресом, дружок')
+    elif AUTHORIZATION_STATE == True:  # препод
         await congstuOut(message)
 
-# Команды -------------------------------------------------------------------------------------------------------------
-# Output функции ------------------------------------------------------------------------------------------------------
-
-
-# Output функции ---------------------------------------------------------------------------------------------------
-# Кнопки -----------------------------------------------------------------------------------------------------------
-# async def BUTTON(message: types.Message):
-#   if message.text == '🛠 Помощь':  # кнопочки, текст внутри message  не трогать
-#      await message.reply("help  is working")
-# elif message.text == '⁉ Частые вопросы':
-#    await faqOut(message)
-# elif message.text == '🧩 Тесты':
-#   await testcheckOut(message)
-# elif message.text == '🔔 Напомнить о занятии':
-#   await remlesOut(message)
-# elif message.text == '🔔 Напомнить о дедлайне':
-#   await remdeadOut(message)
-# elif message.text == '🎓 Успеваемость':
-#   await eduresOut(message)
-# elif message.text == '📚️ Учебный материал':
-#   await edumatOut(message)
-# elif message.text == '🏆 Топ':
-#   await congstuOut(message)
-# elif message.text == 'Другое ➡':
-#   await message.reply("Другое ➡", reply_markup=nav.otherMenu)
-# elif message.text == '⬅ Главное меню':
-#   await message.reply("⬅ Главное меню", reply_markup=nav.mainMenu)
-
-# СТАТУС ПРЕПОДАВАТЕЛЬ СТАТУС ПРЕПОДАВАТЕЛЬ СТАТУС ПРЕПОДАВАТЕЛЬ СТАТУС ПРЕПОДАВАТЕЛЬ СТАТУС ПРЕПОДАВАТЕЛЬ СТАТУС ПРЕПОД
-
+# ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК
 @dp.message_handler()
 async def SCRIPT(message: types.Message):
     if str(message.text[
@@ -313,12 +245,11 @@ async def SCRIPT(message: types.Message):
         if text_has_emoji(str(message.text)) is False:
             await chat(message)  # Код Давида
         elif AUTHORIZATION_STATE == True:
-            await StudBUTTON(message)  # Кнопки Ангелины
+            await BUTTON(message)  # Кнопки Ангелины
         elif AUTHORIZATION_STATE == False:
             await StudBUTTON(message)  # Кнопки Ангелины
-    else:
-        pass
 
+# ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК ОБЩИЙ ОБРАБОТЧИК
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
